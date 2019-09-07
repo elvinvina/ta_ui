de4c34e346bdfdbb5d83f9555df206ad2462df79
elvinvina <vina9196@gmail.com>
2019-09-08 06:33:17 +0700

    nambah layout & login udh bisa ke-launch

--- app/src/main/java/com/ta/prototype2/login.java
+++ app/src/main/java/com/ta/prototype2/login.java
@@ -1,7 +1,10 @@
 package com.ta.prototype2;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class login extends AppCompatActivity {
     public static final String PREFS_NAME = "user";
@@ -11,6 +14,21 @@ public class login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        //SharedPreferences user = (SharedPreferences) getPreferences(PREFS_NAME, 0);
+
+
+
+    }
+
+    public void goRegist(View view) {
+        Intent i = new Intent(login.this, register_first.class);
+        startActivity(i);
+        finish();
+    }
+
+    public void goLogin(View view) {
 
     }
 }
